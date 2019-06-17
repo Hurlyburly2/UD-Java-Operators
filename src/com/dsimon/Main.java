a0b612d08200efd8ee0f49a350bcdbd1167201dc
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-16 20:09:39 -0400

    challenge complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -68,7 +68,23 @@ public class Main {
             System.out.println("wasCar = " + true);
 
 
+        //  1. Create a double variable with the value of 20
+        //  2. Create a second variable of type double with the value of 80
+        //  3. Add both numbers up and multiply by 25
+        //  4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        //  5. Write an "if" statement that displays a message "Total was over the limit"
+        //      If the remaining total (#4) is equal to 20 or less
 
+
+        double firstNumber = 20d;
+        double secondNumber = 80d;
+        double challengeSum = 25 * (firstNumber + secondNumber);
+        System.out.println("challengeSum = " + challengeSum);
+
+        double remainder = challengeSum % 40;
+        System.out.println("remainder = " + remainder);
+        if (remainder <= 20)
+            System.out.println("Total was over the limit.");
 
 
 
